8a7fbe6d6bad5c3ab916d87156e6f4d04428bc95
Jie Hou <jie@pony.ai>
2017-08-27 02:25:33 -0700

    WIP: finish the simplist model

--- train.py
+++ train.py
@@ -7,10 +7,10 @@ from keras.models import Sequential
 from keras.layers import Flatten, Dense, Lambda, Cropping2D
 
 from sklearn.model_selection import train_test_split
+from sklearn.utils import shuffle
 
 
-TRAINING_BATCH_SIZE = 384  # 3 * 128
-VALIDATION_BATCH_SIZE = 128  # 1 * 128
+BATCH_SIZE = 64
 EPOCH_COUNT = 5
 
 
@@ -30,91 +30,81 @@ def read_driving_log(driving_log_path):
     return log_lines
 
 
-def training_data_generator(training_log_lines, batch_size):
+def data_generator(log_lines, batch_size, read_from_log_line):
+    while True:  # fit_generator requires this generator to have infinite loop
+        shuffled_log_lines = shuffle(log_lines)  # Shuffle data before each epoch
+        for line_offset in range(0, len(shuffled_log_lines), batch_size):
+            batch_log_lines = shuffled_log_lines[line_offset: line_offset + batch_size]
+            images = []
+            steerings = []
+            for line in batch_log_lines:
+                read_images, read_steerings = read_from_log_line(line)
+                images.extend(read_images)
+                steerings.extend(read_steerings)
+
+            X_train = np.array(images)
+            y_train = np.array(steerings)
+            yield shuffle(X_train, y_train)  # Shuffle each batch's internal data
+
+
+def read_training_data_from_log_line(line):
     images = []
     steerings = []
-    data_count = 0
 
-    while True:
-        for line in training_log_lines:
-            center_image_path, left_image_path, right_image_path = line[0], line[1], line[2]
+    center_image_path, left_image_path, right_image_path = line[0], line[1], line[2]
 
-            # center image
-            image = cv2.imread(center_image_path)
-            image = process_image(image)
-            steering = float(line[3])
-            images.append(image)
-            steerings.append(steering)
+    # center image
+    image = cv2.imread(center_image_path)
+    image = process_image(image)
+    steering = float(line[3])
+    images.append(image)
+    steerings.append(steering)
 
-            # left image
-            image = cv2.imread(left_image_path)
-            image = process_image(image)
-            steering = float(line[3]) + 1
-            images.append(image)
-            steerings.append(steering)
+    # left image
+    image = cv2.imread(left_image_path)
+    image = process_image(image)
+    steering = float(line[3]) + 1
+    images.append(image)
+    steerings.append(steering)
 
-            # right image
-            image = cv2.imread(right_image_path)
-            image = process_image(image)
-            steering = float(line[3]) - 1
-            images.append(image)
-            steerings.append(steering)
+    # right image
+    image = cv2.imread(right_image_path)
+    image = process_image(image)
+    steering = float(line[3]) - 1
+    images.append(image)
+    steerings.append(steering)
 
-            data_count += 3
-            if data_count == batch_size:
-                yield (np.array(images), np.array(steerings))
-                del images[:]
-                del steerings[:]
-                data_count = 0
-
-        # yield last batch if there's still data
-        if data_count:
-            yield (np.array(images), np.array(steerings))
-
-        del images[:]
-        del steerings[:]
-        data_count = 0
+    return images, steerings
 
 
-def validation_data_generator(validation_log_lines, batch_size):
+def read_validation_data_from_log_line(line):
     images = []
     steerings = []
-    data_count = 0
 
-    while True:
-        for line in validation_log_lines:
-            # only use the center image because we will only use ground truth in validation data
-            center_image_path = line[0]
-            image = cv2.imread(center_image_path)
-            image = process_image(image)
-            steering = float(line[3])
-            images.append(image)
-            steerings.append(steering)
+    # only use the center image because we will only use ground truth in validation data
+    center_image_path = line[0]
+    image = cv2.imread(center_image_path)
+    image = process_image(image)
+    steering = float(line[3])
+    images.append(image)
+    steerings.append(steering)
 
-            data_count += 1
-            if data_count == batch_size:
-                yield (np.array(images), np.array(steerings))
-                del images[:]
-                del steerings[:]
-                data_count = 0
-
-        # yield last batch if there's still data
-        if data_count:
-            yield (np.array(images), np.array(steerings))
-
-        del images[:]
-        del steerings[:]
-        data_count = 0
+    return images, steerings
 
 
 if __name__ == "__main__":
     log_lines = read_driving_log('data_1/driving_log.csv')
     training_log_lines, validation_log_lines = train_test_split(log_lines, test_size=0.2)
 
-    training_generator = training_data_generator(training_log_lines, TRAINING_BATCH_SIZE)
-    validation_generator = validation_data_generator(validation_log_lines, VALIDATION_BATCH_SIZE)
+    training_data_generator = data_generator(training_log_lines, BATCH_SIZE,
+                                             read_training_data_from_log_line)
+    validation_data_generator = data_generator(validation_log_lines, BATCH_SIZE,
+                                               read_validation_data_from_log_line)
 
-    # for batch in training_generator:
+    # for batch in training_data_generator:
+    #     print(batch[0].shape)
+    #     print(batch[1].shape)
+    #     print("")
     #     pass
 
     # for batch in validation_generator:
@@ -132,8 +122,8 @@ if __name__ == "__main__":
     print(model.output_shape)
 
     model.compile(loss='mse', optimizer='adam')
-    model.fit_generator(training_generator, len(training_log_lines) * 3, nb_epoch=5,
-                        validation_data=validation_generator,
+    model.fit_generator(training_data_generator, len(training_log_lines) * 3, nb_epoch=EPOCH_COUNT,
+                        validation_data=validation_data_generator,
                         nb_val_samples=len(validation_log_lines))
 
     model.save('model.h5')
